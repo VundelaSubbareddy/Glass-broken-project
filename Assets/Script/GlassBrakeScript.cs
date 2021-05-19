43ad483bfad2fe1bb4d53f8ce0ee7b0dc20a2c69
Vundela Subbareddy <82031292+VundelaSubbareddy@users.noreply.github.com>
2021-05-19 12:23:26 +0530

    Add files via upload

---
+++ Assets/Script/GlassBrakeScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlassBrakeScript : MonoBehaviour
+{
+    public Transform prefab;
+    public float Force = 1f;
+    public float radius, power, upwards;
+    public AudioSource Audioclip;
+   // public AudioClip audiO;
+
+    
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.relativeVelocity.magnitude > Force)
+        {
+            Destroy(gameObject);
+            Instantiate(prefab,transform.position,transform.rotation);
+            prefab.localScale = transform.localScale;
+           // Audioclip.GetComponent<AudioSource>().playOnAwake = enabled;
+            Vector3 explosionPos = transform.position;
+            Collider[] colliders = Physics.OverlapSphere(explosionPos, radius);
+            foreach (Collider hit in colliders)
+            {
+                if (hit.gameObject.GetComponent<Rigidbody>())
+                {
+                    hit.gameObject.GetComponent<Rigidbody>().isKinematic = false;
+                    hit.gameObject.GetComponent<Rigidbody>().AddExplosionForce(power * col.relativeVelocity.magnitude, explosionPos, radius, upwards);
+                   AudioSource Audioclp = GetComponent<AudioSource>();
+                    Audioclp.Play();
+                }
+
+            }
+                Debug.Log("prefabActive");
+        }
+        Debug.Log("Sphere ToucH");
+    }
+}
---
+++ Assets/Script/Gunfire.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gunfire : MonoBehaviour
+{
+    public Transform firepos;
+    Rigidbody rd;
+    public float firetime = 2f;
+    private float timer = 0.01f;
+    public GameObject prefab;
+    public float Range = 20f;
+
+
+    void Start()
+    {
+        rd = GetComponent<Rigidbody>();
+
+    }
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            firepoS();
+           
+        }
+       
+    }
+    void firepoS()
+    {
+        Debug.Log("fire");
+        GameObject g = Instantiate(prefab, firepos.transform.position,firepos.transform.rotation);
+        Rigidbody rb = g.GetComponent<Rigidbody>();
+        rb.velocity = new Vector3(0,0,Range);
+       // rb.AddForce(Vector3.forward * 10f);
+        Destroy(g , 1f);
+    }
+}
